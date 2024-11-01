db06b9c149968439e8e2d7cd383127f2303e00d6
Velin Dankov <velin80@web.de>
2024-11-01 14:48:35 +0200

    create list and inside item folder and components inside

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -5,8 +5,29 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './user-list.component.html',
-  styleUrl: './user-list.component.css'
+  styleUrl: './user-list.component.css',
 })
 export class UserListComponent {
-
+  users = [
+    {
+      name: 'Pescho',
+      id: 1,
+      age: 22,
+    },
+    {
+      name: 'Ivan',
+      id: 2,
+      age: 24,
+    },
+    {
+      name: 'Velin',
+      id: 3,
+      age: 25,
+    },
+    {
+      name: 'Gancho',
+      id: 4,
+      age: 26,
+    },
+  ];
 }
